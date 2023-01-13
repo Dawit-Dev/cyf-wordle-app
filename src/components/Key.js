edee3eb11eb6e569e97b526c8e717f290087c5b4
Dawit Abraha <akdawit1@gmail.com>
2023-01-13 20:32:31 +0000

    Added keyboard and board

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../App";
 
-const Key = ({ keyVal }) => {
+const Key = ({ keyVal, bigKey }) => {
+  const { onDelete, onSelectLetter, onEnter } = useContext(AppContext);
+
+  const selectLetter = () => {
+    if (keyVal === "ENTER") {
+      onEnter();
+    } else if (keyVal === "DELETE") {
+      onDelete();
+    } else {
+      onSelectLetter(keyVal);
+    }
+  };
+
   return (
-    <div className='key'>
+    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
       {keyVal}
     </div>
-  )
-}
+  );
+};
 
-export default Key
+export default Key;
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,13 +1,49 @@
-import React from "react";
-import Key from "./Key";
+import React, { useContext, useCallback, useEffect } from "react";
+import { AppContext } from "../App";
+import Key from './Key'
 
 const Keyboard = () => {
+  const { onEnter, onDelete, onSelectLetter } = useContext(AppContext)
+
+
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
+  const handleKeyboard = useCallback((event) => {
+    if (event.key === 'Enter') {
+      
+    } else if (event.key === 'Backspace') {
+      onDelete();
+    } else {
+      keys1.forEach((key) => {
+        if (event.key === key) {
+          onSelectLetter(key)
+        }
+      })
+      keys2.forEach((key) => {
+        if (event.key === key) {
+          onSelectLetter(key)
+        }
+      })
+          keys3.forEach((key) => {
+            if (event.key === key) {
+              onSelectLetter(key)
+            }
+          })
+        }
+      })
+
+  useEffect(() => {
+    document.removeEventListener('keydown', handleKeyboard);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyboard);
+    }
+  }, [handleKeyboard])
+
   return (
-    <div className="keyboard">
+    <div className="keyboard" onKeyDown={handleKeyboard}>
       <div className="line1">
         {keys1.map((key) => {
           return <Key keyVal={key} />;
@@ -19,9 +55,11 @@ const Keyboard = () => {
         })}
       </div>
       <div className="line3">
+        <Key keyVal={"ENTER"} bigKey />
         {keys3.map((key) => {
           return <Key keyVal={key} />;
         })}
+        <Key keyVal={"DELETE"} bigKey />
       </div>
     </div>
   );
